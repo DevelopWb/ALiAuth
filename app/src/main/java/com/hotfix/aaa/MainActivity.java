eac89a6bd50468be214a86dbd55e44ae64fa5ed8
DevelopWb <bin_w_ing@foxmail.com>
2021-02-08 17:28:45 +0800

    auth

---
+++ app/src/main/java/com/hotfix/aaa/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hotfix.aaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * @aouther tobato
+ * @description 描述  一键认证
+ * @date 2021/2/8 16:41
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
